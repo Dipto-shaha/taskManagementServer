49d20bf7ade435ddaad2603f1ca893672b44a085
Dipto Shaha <diptoshaha2635@gmail.com>
2023-12-22 10:50:57 +0600

    User Added in DB

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require('dotenv').config()
+
+const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
+
+const port= process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.SECRET_KEY}@cluster0.bnmqd0w.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    //await client.connect();
+    // Send a ping to confirm a successful connection
+    const User = client.db("toDoList").collection("user");
+    const todolist = client.db("toDoList").collection("toDoList");
+
+
+    //await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  
+    //Add user
+    app.post("/adduser", async (req, res) => {
+      const userInfo = req.body;
+      const result = await User.findOne({email:userInfo.email});
+      if(result) res.send(result);
+      else
+      {
+        const result = await User.insertOne(userInfo);
+        res.send(result);
+      }
+      
+    });
+
+    app.get("/check",(req,res)=>{
+      res.send("Database Problem");
+    })
+    
+  
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/',(req,res)=>{
+    res.send('Hello');
+})
+
+app.listen(port, () => {
+    console.log('Server is runding on ',port);
+})
+
+
